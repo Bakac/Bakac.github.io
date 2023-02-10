70713fa654fea8b781b6c69ff5c4658eadd5b485
Bakac <frankiemak0610@hotmail.com>
2023-02-10 12:13:46 +0800

    Update: Slideshow and CUHK map

--- js/home.js
+++ js/home.js
@@ -1,59 +1,88 @@
+//import { Splide, SplideSlide } from 'https://cdn.jsdelivr.net/npm/@splidejs/vue-splide@0.6.12/+esm'
+import { Splide, SplideSlide } from './vuesplide.js'
+
 export default {
-  props: {
-    labname: String
-  },
-  data() {
-    return {
-      
-    }
-  },
-  template: `
-      <main>
-        <section id="home-research" class="banner">
-          <div id="home-research-flex" class="wrapper-horizontal flex-box align-center">
-            <div class="banner-overlay left-align flex-40">
-              <h1>How do we process relations with others and the self?</h1>
-              <span class="home-subtitle">We are interested in the cognitive bases and social development involved in relational processing, which are important for understanding trauma-related disorders.</span>
-              <button class="home-btn center-align" @click.prevent="$emit('clickPage', './?page=research')">Learn More</span>
-            </div>
-            <div id="home-research-banner" class="flex-40">
-              <img src="./img/home-banner.webp" width="1920" alt="">
-            </div>
-          </div>
-        </section>
+	components: {
+		Splide, SplideSlide
+	},
+	props: {
+		labname: String
+	},
+	data() {
+		return {
+			
+		}
+	},
+	template: `
+			<main>
+				<section id="home-research" class="banner">
+					<div id="home-research-flex" class="wrapper-horizontal flex-box align-center">
+						<div class="banner-overlay left-align flex-40">
+							<h1>How do we process relations with others and the self?</h1>
+							<span class="home-subtitle">We are interested in the cognitive bases and social development involved in relational processing, which are important for understanding trauma-related disorders.</span>
+							<button class="home-btn center-align" @click.prevent="$emit('clickPage', './?page=research')">Learn More</span>
+						</div>
+						<div id="home-research-banner" class="flex-40">
+							<img src="./img/home-banner.webp" width="1920" alt="">
+						</div>
+					</div>
+				</section>
 
-        <section class="body-container">
-          <section id="home-people" class="wrapper-horizontal flex-box">
-            <div class="flex-40">
-              <h2>Our Team</h2>
-              <p>Our laboratory includes research staff, research students, clinical students, and clinical psychologists who are now working in different settings.</p>
-              <button class="center-align" @click.prevent="$emit('clickPage', './?page=people')">Meet The Team</span>
-            </div>
-            <div class="flex-60"><img src="./img/lab-photo.webp" width="1920" alt=""></div>
-          </section>
+				<section class="body-container">
+					<section id="home-people" class="wrapper-horizontal flex-box">
+						<div class="flex-40">
+							<h2>Our Team</h2>
+							<p>Our laboratory includes research staff, research students, clinical students, and clinical psychologists who are now working in different settings.</p>
+							<button class="home-btn center-align" @click.prevent="$emit('clickPage', './?page=people')">Meet The Team</span>
+						</div>
+						<div class="flex-60">
+							<splide :options="{ type: 'fade', rewind: true, speed: 2000, arrows: false, drag: false, autoplay: true, interval: 4000, pauseOnHover: false, pauseOnFocus: false, lazyLoad: 'sequential' }" aria-label="Services">
+								<splide-slide><img src="./img/lab-photo-2022-06.jpg" width="1920" alt=""></splide-slide>
+								<splide-slide><img data-splide-lazy="./img/lab-photo-2018-12.jpg" width="1920" alt=""></splide-slide>
+								<splide-slide><img data-splide-lazy="./img/lab-photo-2017-12.jpg" width="1920" alt=""></splide-slide>
+								<splide-slide><img data-splide-lazy="./img/lab-photo-2017-09.jpg" width="1920" alt=""></splide-slide>
+								<splide-slide><img data-splide-lazy="./img/lab-photo-2016-10.jpg" width="1920" alt=""></splide-slide>
+							</splide>
+						</div>
+					</section>
 
-          <section id="home-services" class="wrapper-horizontal flex-box flex-box-reverse">
-            <div class="flex-40">
-              <h2>Our Services</h2>
-              <p>We provide a wide range of services to the community and service units, including training workshops and mental health service delivery model development.</p>
-              <button class="center-align" @click.prevent="$emit('clickPage', './?page=services')">Learn More</span>
-            </div>
-            <div class="flex-60"><img src="./img/service-photo.jpg" width="1920" alt="" loading="lazy"></div>
-          </section>
+					<section id="home-services" class="wrapper-horizontal flex-box flex-box-reverse">
+						<div class="flex-40">
+							<h2>Our Services</h2>
+							<p>We provide a wide range of services to the community and service units, including training workshops and mental health service delivery model development.</p>
+							<button class="home-btn center-align" @click.prevent="$emit('clickPage', './?page=services')">Learn More</span>
+						</div>
+						<div class="flex-60">
+							<splide :options="{ type: 'fade', rewind: true, speed: 2000, arrows: false, drag: false, autoplay: true, interval: 4000, pauseOnHover: false, pauseOnFocus: false, lazyLoad: 'sequential' }" aria-label="Services">
+								<splide-slide><img data-splide-lazy="./img/service-photo-2022-12-1.jpg" width="1920" alt=""></splide-slide>
+								<splide-slide><img data-splide-lazy="./img/service-photo-2022-12-2.jpg" width="1920" alt=""></splide-slide>
+								<splide-slide><img data-splide-lazy="./img/service-photo-2022-12-3.jpg" width="1920" alt=""></splide-slide>
+							</splide>
+						</div>
+					</section>
 
-          <section id="contact" class="wrapper-horizontal flex-box">
-            <div class="flex-40">
-              <h2>Contact Us</h2>
-              <p>Room 102, Wong Foo Yuan Building<br>
-              The Chinese University of Hong Kong<br>
-              Shatin, N.T.<br>
-              Hong Kong SAR</p>
-              <p>Tel: 3943-3507</p>
-            </div>
-            <div class="flex-60">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3688.3732361161433!2d114.20527311504736!3d22.414973644386393!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3404089d8c0d8623%3A0xcceaf3d289427bbd!2z5rKZ55Sw5bSH5Z-66Lev546L56aP5YWD5qiT!5e0!3m2!1sen!2sen!4v1670808888909!5m2!1sen!2sen" width="500" height="300" style="border:0; margin: 0 auto" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            </div>
-          </section>
-        </section>
-      </main>`
+					<section id="contact" class="wrapper-horizontal flex-box">
+						<div class="flex-60">
+							<h2>Contact Us</h2>
+
+							<p>臨床心理科學實驗室<br>
+							香港新界沙田香港中文大學王福元樓102室</p>
+
+							<p>Experimental Psychopathology and 
+							<br>Psychotherapy Laboratory</p>
+
+							Room 102, Wong Foo Yuan Building<br>
+							The Chinese University of Hong Kong<br>
+							Shatin, N.T., Hong Kong SAR</p>
+							
+							<p>Tel: 3943-3507</p>
+						</div>
+						<div class="flex-40">
+							<img src="./img/cuhk-map.webp" width="1920" alt="" loading="lazy">
+						</div>
+					</section>
+				</section>
+			</main>
+
+			<link href="https://cdn.jsdelivr.net/npm/@splidejs/vue-splide@0.6.12/dist/css/splide.min.css" rel="stylesheet">`
 }
