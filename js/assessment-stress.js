a7b2c92e671085807400773b208eb0cbc0b9e007
Bakac <frankiemak0610@hotmail.com>
2023-03-20 09:27:00 +0800

    Update: assessment

--- js/assessment-stress.js
+++ js/assessment-stress.js
@@ -24,14 +24,21 @@ export default {
 
 				<div class="margin-top-64">
 			    	<h2>Reactions to stressful events and self-representation</h2>
-			    	<p>Imagine that you have lost your close friend in an accident. This would normally induce great emotional reactions. Being the person who witnessed the accident, some people would often have the experience of the accident popping up in the mind. The experience is often vivid and detailed, as if they were in the event again. Some people would think of the event all day long, such as its causes and consequences. There are also people who have both experiences, and people who would recover from the event in a short time.<br></p>
+			    	<p>Imagine that you have lost your close friend in an accident. This would normally induce great emotional reactions. Being the person who witnessed the accident, some people would often have the experience of the accident popping up in the mind. The experience is often vivid and detailed, as if they were in the event again. Some people would think of the event all day long, such as its causes and consequences. There are also people who have both experiences, and people who would recover from the event in a short time. The reactions to stressful events such as the above can vary greatly across people. We postulate that the type of reactions is related to their underlying self-representation, which operates on two memory systems.<br></p>
 
-			    	<p>TThe reactions to stressful events such as the above can vary greatly across people. We postulate that the type of reactions is related to their underlying self-representation, which operates on two memory systems. <br><br><br></p>
+					<h2 class="margin-top-64">Flashback and event-based autobiographical memory</h2>
+			    	<p>[under constructoion]<br></p>
+			    	<h3 class="toggleBtn highlight" @click.prevent="$emit('clickPage', './?page=project-2')"><i class="fa fa-chevron-right toggle-icon"></i>Related research: Distorted Self-Referential Memory in Trauma-Related Disorders</h3>
 
+					<h2 class="margin-top-64">Preoccupation and script-based autobiographical knowledge</h2>
+			    	<p>[under constructoion]<br></p>
+			    	<h3 class="toggleBtn highlight" @click.prevent="$emit('clickPage', './?page=project-1')"><i class="fa fa-chevron-right toggle-icon"></i>Related research: Flexible Perspective Shifting in Empathy and Self-Empathy</h3>
 
-			    	<p>[Talk about two types of memories, and frame them as source memory and VSPT. Talk about shame-guilt, self-reassurance, and responsiveness in close relationship. Talk about stress-related disorders. ]</p>
+			    	<h2 class="margin-top-64">Self-relating style, relationships, and stress-related disorders</h2>
+			    	<p>[under constructoion: Talk about shame-guilt, self-reassurance, and responsiveness in close relationship.]<br></p>
+			    	<h3 class="toggleBtn highlight" @click.prevent="$emit('clickPage', './?page=project-3')"><i class="fa fa-chevron-right toggle-icon"></i>Related research: Biased Self-Evaluation in Trauma-Related Disorders</h3>
 
-			    	<p>[Further readings: move some contents from research highlights]</p>
+			    	<p> <br><br><br></p>
 
 
 			    	<p style="text-align: center">[finally, a button asking them to take the test]</p>
